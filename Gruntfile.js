3ab043b5c434fcf8f7d8cedbefc07bda28df2c8b
Emre Surmeli <s.emre.s.8@gmail.com>
2015-04-29 11:13:49 -0700

    grunt packadge jshintrc

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    simplemocha: {
+      
+      dev: {
+        src: ['test/**/*.js']
+      },
+    },
+    
+    jshint: {
+      
+      dev: {
+        src: ['Gruntfile.js', 'greet*.js', 'test/*.js']
+      },
+
+      options: {
+        node: true,
+        globals: {
+          describe: true,
+          it: true,
+          before: true,
+          after: true,
+          beforeEach: true,
+          afterEach: true
+        }
+      }
+    },
+
+    watch: {
+      files: ['test/*.js', 'greet*.js'],
+      tasks: ['jshint:dev', 'simplemocha:dev']
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-simple-mocha');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  //register the default task
+  grunt.registerTask('default', ['test']);
+  grunt.registerTask('test', ['jshint:dev', 'simplemocha:dev']);
+};
+
